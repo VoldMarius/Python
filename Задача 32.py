d0346a5b14334a23df1769bd4f70a57e9a777cde
VoldMarius <ukusi@mail.ru>
2023-02-13 12:53:21 +0300

    Домашка 6 семинара

--- Задача 32.py
+++ Задача 32.py
@@ -2,16 +2,17 @@
 # меньше заданного минимума и не больше заданного максимума)
 # Ввод: [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
 # Вывод: [1, 9, 13, 14, 19]
+from module import creat_list
 import os
 
 os.system('cls')
-from module import creat_list
 
 num_list = creat_list(10)
 minimum = int(input('Введите минимальный элемент интервала:  '))
 maximum = int(input('Введите максимальный элемент интервала:  '))
 new = []
 for i in range(len(num_list)):
-    if num_list[i] >= minimum and num_list[i] <= maximum:
-        new.append(i)
+    if num_list[i] > minimum and num_list[i] < maximum:
+        new.append(num_list[i])
+print(num_list)
 print(new)
